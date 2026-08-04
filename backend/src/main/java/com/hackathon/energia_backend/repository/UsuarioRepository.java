e057b2739248d85a8079e7d31d6ab4cfa424bf88
Angel Melendez <angel99.melendez@gmail.com>
2026-08-04 02:48:13 -0600

    feat(backend): Add the interface for the User entity repository

---
+++ backend/src/main/java/com/hackathon/energia_backend/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.hackathon.energia_backend.repository;
+
+import com.hackathon.energia_backend.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+}
