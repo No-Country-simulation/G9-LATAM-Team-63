867e99d05d02d0234b2bae98e168aff8b47984c8
Angel Melendez <angel99.melendez@gmail.com>
2026-08-04 03:01:27 -0600

    feat(backend): add methods, lines of code, and import interfaces to the SecurityConfig class

--- backend/src/main/java/com/hackathon/energia_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/hackathon/energia_backend/config/SecurityConfig.java
@@ -5,9 +5,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -34,6 +39,8 @@ import java.util.Arrays;
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
 
     /**
      * Configura y personaliza la cadena de filtros de seguridad HTTP.
@@ -69,6 +76,7 @@ public class SecurityConfig {
                 // =====================================================================
                 .authorizeHttpRequests(auth -> auth
                         // 3.1. Rutas PÚBLICAS: No requieren token JWT
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()// Permitir acceso público a /login
                         .requestMatchers(
                                 "/swagger-custom.html",
                                 "/v3/api-docs/**",
@@ -145,5 +153,15 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
